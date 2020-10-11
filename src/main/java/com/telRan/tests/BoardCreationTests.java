74343b5aa7d8f9c0c4a7b5b796321c05c40724dd
kathy07-max <ksheva0209@gmail.com>
2020-10-11 11:00:46 +0300

    extends

---
+++ src/main/java/com/telRan/tests/BoardCreationTests.java
@@ -0,0 +1,20 @@
+package com.telRan.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class BoardCreationTests extends TestBase{
+
+    @Test
+    public void boardCreation(){
+        clickOnPlusButton();
+        //selectCreateBoard();
+        //fillBoardForm();
+        //confirmBoardCreation();
+        //returnOnHomePage();
+    }
+
+    public void clickOnPlusButton() {
+        click(By.cssSelector(""));
+    }
+}
--- src/main/java/com/telRan/tests/LoginTrelloTest.java
+++ src/main/java/com/telRan/tests/LoginTrelloTest.java
@@ -1,52 +1,33 @@
 package com.telRan.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class LoginTrelloTest extends TestBase {
 
-public class LoginTrelloTest {
-    WebDriver wd;
-
-    @BeforeClass
-    public void setUp(){
-        //create new instance of WebDriver (ex. open Chrom Browser)
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.manage().window().maximize();
-        wd.navigate().to("https://trello.com/");
-
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (isAvatarPresent()) {
+            logout();
+        }
     }
 
-   @Test
+    @Test
     public void loginAtlassianAccPositiveTest() throws InterruptedException {
-        //click on login button on welcome page
-       wd.findElement(By.cssSelector("[href='/login']")).click();
-       //fill login form
-       //fill email
-       wd.findElement(By.name("user")).click();
-       wd.findElement(By.name("user")).clear();
-       wd.findElement(By.name("user")).sendKeys("masuha02@mail.ru");
-       //wait
-       
-       //click on login with atlassian button
-       wd.findElement(By.id("login")).click();
-       Thread.sleep(1000);
-       //fill password
-       wd.findElement(By.id("password")).click();
-       wd.findElement(By.id("password")).clear();
-       wd.findElement(By.id("password")).sendKeys("2871539@com");
-       //confirm login
-       wd.findElement(By.id("login-submit")).click();
-    // er user logged in (is avatar present)
+        login("masuha02@mail.ru", "2871539@com");
+        Assert.assertTrue(isAvatarPresent());
 
     }
-   @AfterClass (enabled = false)
-    public void tearDown(){
-        wd.quit();
+
+    @Test
+    public void negativeLoginAtlassianAccPositiveTest() throws InterruptedException {
+        login("masuha02@mail.ru", "2871539@cov");
+        Assert.assertTrue(isLogInErrorPresent());
+        Assert.assertFalse(isAvatarPresent());
+
     }
 }
+
+
+
